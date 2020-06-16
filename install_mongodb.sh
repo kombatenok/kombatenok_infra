31eacac6bc194dfebf0b3d8e80e2e0f1ce123b85
kombatenok <kkombatenok@gmail.com>
2020-06-16 18:01:31 +0300

    adding in services mongodb

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -4,3 +4,5 @@ sudo bash -c 'echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.
 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.2.list'
 sudo apt -y update
 sudo apt install -y mongodb
+sudo systemctl start mongodb
+sudo systemctl enable mongodb
