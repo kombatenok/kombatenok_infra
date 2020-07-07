4faf2e20ee7b91ac446a498f56c42a25542a4f82
glotovdanila <glotovdanila@gmail.com>
2020-07-07 16:05:17 +0000

    final

--- packer/scripts/deploy.sh
+++ packer/scripts/deploy.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+echo "Hello"
 git clone -b monolith https://github.com/express42/reddit.git
 cd reddit && bundle install
 puma -d
+mv /home/kombatenok/puma.service /etc/systemd/system/puma.service
+systemctl start puma
+systemctl enable puma
 ###
